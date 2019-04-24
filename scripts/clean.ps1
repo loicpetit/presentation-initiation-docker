c2544eec1ad3c2adba72f92e36e5ebe1e8b0c36d
Loic PETIT <loic.petit@capgemini.com>
2019-04-24 17:30:56 +0200

    Etape 3 ajout du reseau

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -2,5 +2,6 @@ docker container stop sf-food-trucks-webapp
 docker container stop sf-food-trucks-es
 docker container rm sf-food-trucks-webapp
 docker container rm sf-food-trucks-es
+docker network rm sf-food-trucks-net
 #docker image rm docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 #docker image rm sf-food-trucks-webapp
--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,3 +1,13 @@
-docker build -t sf-food-trucks-webapp flask-app
+# Creation image
+docker build -t sf-food-trucks-webapp ../flask-app
+
+# Creation réseau
+docker network create sf-food-trucks-net
+
+# Création conteneurs
 docker run -d --name sf-food-trucks-es -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
-docker run -d --name sf-food-trucks-webapp sf-food-trucks-webapp
+docker run -d --name sf-food-trucks-webapp -p 5000:5000 sf-food-trucks-webapp
+
+# Ajout au réseau
+docker network connect sf-food-trucks-net sf-food-trucks-webapp
+docker network connect --alias es sf-food-trucks-net sf-food-trucks-es
