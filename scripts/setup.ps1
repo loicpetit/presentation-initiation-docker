ac960e648813d2f160e0cf1f6b4cead274a5a5cc
Loic PETIT <loic.petit@capgemini.com>
2019-04-24 17:32:13 +0200

    fix path build context dans etup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,3 +1,3 @@
-docker build -t sf-food-trucks-webapp flask-app
+docker build -t sf-food-trucks-webapp ../flask-app
 docker run -d --name sf-food-trucks-es -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 docker run -d --name sf-food-trucks-webapp sf-food-trucks-webapp
