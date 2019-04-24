cd0e885376d126c084b31f7325c80472a5e61a9e
Loic PETIT <loic.petit@capgemini.com>
2019-04-24 17:12:36 +0200

    Etape 2 elastic search

---
+++ scripts/clean.ps1
@@ -0,0 +1,6 @@
+docker container stop sf-food-trucks-webapp
+docker container stop sf-food-trucks-es
+docker container rm sf-food-trucks-webapp
+docker container rm sf-food-trucks-es
+#docker image rm docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+#docker image rm sf-food-trucks-webapp
